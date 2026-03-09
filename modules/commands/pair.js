f21d1d009c0282d78dc114b18ece518ce5ce40ad
kvishal68483-ui <kvishal68483@gmail.com>
2026-03-09 18:12:48 +0530

    Update pair.js

--- modules/commands/pair.js
+++ modules/commands/pair.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pair",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝙑𝙞𝙠𝙖𝙨 𝙍𝙖𝙟𝙥𝙪𝙩",
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭,
   description: "It's a compound :>",
   commandCategory: "Giải trí",
   usages: "",
